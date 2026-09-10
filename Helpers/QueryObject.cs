36cdc44f88de63ee03f60528c2d1ae9865f78a92
sah1384epm <seyedalihos84@gmail.com>
2026-09-10 11:04:01 +0330

    feat(stock): implement pagination in QueryObject- Add PageNumber and PageSize properties to QueryObject- Implement Skip and Take logic in StockRepository- Set default values for page number and size

--- Helpers/QueryObject.cs
+++ Helpers/QueryObject.cs
@@ -6,5 +6,7 @@ namespace API.Helpers
         public string? CompanyName { get; set; } = null;
         public string? SortBy { get; set; } = null;
         public bool IsDecending { get; set; } = false;
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 20;
     }
 }
--- obj/Debug/net10.0/API.AssemblyInfo.cs
+++ obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81c88801eba0d982b801672d268c2e7ebb5c7049")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+427f757f25dc940f5214ae9f7aa0c95805d2fcd0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
