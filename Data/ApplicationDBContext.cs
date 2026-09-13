52ec030d7043f6d1a8c86ba4cffb62fe568eac8e
sah1384epm <seyedalihos84@gmail.com>
2026-09-13 14:39:38 +0330

    feat(auth): implement user registration functionality- Add RegisterDto for user creation payload- Implement Register endpoint in AccountController using UserManager- Seed Admin and User roles in ApplicationDBContext- Add SeedRoles migration and update database

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -1,25 +1,37 @@
-﻿using API.Models;
+﻿namespace API.Data;
+using API.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Data
+public class ApplicationDBContext : IdentityDbContext<AppUser>
 {
-    public class ApplicationDBContext : IdentityDbContext<AppUser>
+    public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
+        : base(options)
     {
-        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options)
-            : base(options)
+    }
+
+    public DbSet<Stock> Stocks { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        List<IdentityRole> roles = new List<IdentityRole>
         {
-        }
+            new IdentityRole
+            {
+                Name = "Admin",
+                NormalizedName = "ADMIN"
+            },
+            new IdentityRole
+            {
+                Name = "User",
+                NormalizedName = "USER"
+            },
+        };
 
-        public DbSet<Stock> Stocks { get; set; }
-        public DbSet<Comment> Comments { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            base.OnModelCreating(builder);
-
-           
-        }
+        builder.Entity<IdentityRole>().HasData(roles);
     }
 }
---
+++ Migrations/20260913110327_SeedRoles.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "2ef06410-8e41-41f9-8d8f-de50dfccfa75", "b1da185f-52ca-4d0b-9aab-f223df315581", "User", "USER" },
+                    { "7f4cebc3-60db-461b-b656-dcacd977048d", "443a3d86-a09e-4f8a-8f8d-968136888fdb", "Admin", "ADMIN" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2ef06410-8e41-41f9-8d8f-de50dfccfa75");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "7f4cebc3-60db-461b-b656-dcacd977048d");
+        }
+    }
+}
